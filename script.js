cd64a57bad9025b7aef359eb0b36639f079fc04a
horaaa777 <ohhhahh5@gmail.com>
2024-11-26 20:35:53 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-// Event listener untuk menangani pengiriman formulir
-document
-  .getElementById("orderForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Mencegah pengiriman form default
-
-    // Ambil nilai dari input formulir
-    const name = document.getElementById("name").value;
-    const phone = document.getElementById("phone").value;
-    const menu = document.getElementById("menu").value;
-
-    let message = document.getElementById("message");
-    message.innerHTML = ""; // Reset pesan sebelumnya
-
-    // Validasi formulir
-    if (name === "" || phone === "" || menu === "") {
-      message.innerHTML =
-        '<p class="error-message">Semua bidang harus diisi!</p>';
-    } else {
-      message.innerHTML =
-        '<p class="success-message">Pemesanan berhasil dikirim! Terima kasih telah memesan di Restoran Lezat.</p>';
-      // Reset form setelah submit
-      document.getElementById("orderForm").reset();
-    }
-  });
+function orderNow() {
+  window.open(
+    "https://docs.google.com/forms/d/e/1FAIpQLSeNLhoWunYaIjPQ-jBMC3YTynY0lPsba-UdEiVNcruIBhuywA/viewform?usp=sf_link",
+    "_blank"
+  );
+}
